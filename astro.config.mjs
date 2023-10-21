fa566496596ea1cc87544ffa19fa81c6127d8919
cf2013 <carlosfrancisco.rg@gmail.com>
2023-10-21 12:39:01 -0600

    sync with frontend owner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind(), react()]
-});
+  compressHTML: true,
+  integrations: [mdx(), tailwind({
+    applyBaseStyles: false,
+  }), compress()],
+})
