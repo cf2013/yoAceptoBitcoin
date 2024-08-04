22dc2f7f7338d2b81b57c44a1c6464b42d6d025d
cf2013 <carlosfrancisco.rg@gmail.com>
2024-08-04 08:13:41 -0600

    undo blur

--- src/scripts/textInput.jsx
+++ src/scripts/textInput.jsx
@@ -1,4 +1,3 @@
-// TextInput.jsx
 import { h } from 'preact';
 import { useEffect, useRef } from 'preact/hooks';
 
@@ -6,33 +5,24 @@ const TextInput = () => {
   const inputRef = useRef(null);
 
   useEffect(() => {
-    const handleFocus = (event) => {
-      event.target.select();
-      event.target.classList.add('blinking');
-    };
-
-    const handleBlur = (event) => {
-      event.target.classList.remove('blinking');
-    };
+    const handleFocus = (event) => event.target.select();
 
     const input = inputRef.current;
     input.addEventListener('focus', handleFocus);
-    input.addEventListener('blur', handleBlur);
 
     return () => {
       input.removeEventListener('focus', handleFocus);
-      input.removeEventListener('blur', handleBlur);
     };
   }, []);
 
   return (
-    <div class="text-input">
-      <input
-        type="text"
-        value="Buscar sitios que aceptan Bitcoin..."
-        ref={inputRef}
-      />
-    </div>
+        <div class="text-input">
+          <input
+            type="text"
+            value="Buscar sitios que aceptan Bitcoin..."
+            ref={inputRef}
+          />
+        </div>
   );
 };
 
