128c7cb4987ee40c0cadce4ff56431d335cb9ce7
cf2013 <carlosfrancisco.rg@gmail.com>
2024-08-04 14:55:44 -0600

    fetch data from cosmos dynamically

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,8 +1,12 @@
 // cosmos.config.js
+// Load environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
     endpoint: process.env.COSMOSDB_ENDPOINT,
     key: process.env.COSMOSDB_KEY,
-    databaseId: "ToDoList",
-    containerId: "items"
+    databaseId: "YoAceptoBitcoinCompanies",
+    containerId: "Companies"
   };
-  
+console.log("CosmosDB Config:", config);
